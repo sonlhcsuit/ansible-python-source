f87073d02c9e5674ade70323b724165c3c149bc6
Lưu Hoàng Sơn <sonlh.cs.uit@gmail.com>
2021-10-21 11:17:19 +0700

    update requirements

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import toml
 import os
 
 if __name__ == '__main__':
-	conf = toml.load(os.path.join(os.getcwd(), 'config', 'config.toml', ))
-	app_conf = conf.get("application")
-	app.run(port=app_conf.get("port"), host=app_conf.get("host"), debug=False)
+    conf = toml.load(os.path.join(os.getcwd(), 'config', 'config.toml', ))
+    app_conf = conf.get("application")
+    app.run(port=app_conf.get("port"), host=app_conf.get("host"), debug=False)
